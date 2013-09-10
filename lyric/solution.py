7f90ca8b258841edd7479b0c37a72a99eeabb989
luminita <lumi.moruz@gmail.com>
2013-09-10 17:09:51 +0200

    Serves as abstract class for a solution

---
+++ lyric/solution.py
@@ -0,0 +1,14 @@
+"""
+This file provides the super class for providing a solution. All classes 
+that give a solution should inherit from this one
+"""
+
+
+class Solution:
+    
+    @staticmethod    
+    def get_solution(words, cache_dict, playlist_base):
+        """ Giving a list of words, and a dictionary including previous searches, 
+        this method should return a list of pairs (song_name, spotify_online_link) """        
+        pass
+
